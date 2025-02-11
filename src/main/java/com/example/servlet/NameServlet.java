ab3bdc01907c1eb8fefbdd70394a0facb4244c08
LeHuuTien002 <lehuutien27022002.com>
2025-02-11 12:16:00 +0700

    push code

---
+++ src/main/java/com/example/servlet/NameServlet.java
@@ -0,0 +1,18 @@
+package com.example.servlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("/name")
+public class NameServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String name = req.getParameter("name");
+        req.setAttribute("name", name);
+        req.getRequestDispatcher("/displayName.jsp").forward(req, resp);
+    }
+}
